2853ef13ef4727ade527aeb8342a2ab5b6c6e2d9
Gastygas <gastonrodrigogonzalez55@gmail.com>
2026-02-21 13:19:44 -0300

    refactor: changes suggested by CodeRabbit

--- src/features/candidate/api/candidate.api.ts
+++ src/features/candidate/api/candidate.api.ts
@@ -5,7 +5,8 @@ export const getCandidateByEmail = async (
   email: string
 ): Promise<Candidate> => {
   const { data } = await api.get<Candidate>(
-    `/api/candidate/get-by-email?email=${email}`
+    `/api/candidate/get-by-email`,
+    { params: { email } }
   );
   return data;
 };
